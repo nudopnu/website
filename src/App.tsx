2227241fa27449208e818dc6544c5055473f6296
Peter Schramm <peter_schramm@live.de>
2023-12-01 13:52:26 +0100

    Integrated react-notion-x

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,56 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+import React from "react";
+import { NotionRenderer } from 'react-notion-x';
+import { Code } from 'react-notion-x/build/third-party/code';
+import { Collection } from 'react-notion-x/build/third-party/collection';
+import { Equation } from 'react-notion-x/build/third-party/equation';
+import { Modal } from 'react-notion-x/build/third-party/modal';
+import { Pdf } from 'react-notion-x/build/third-party/pdf';
+import 'react-notion-x/src/styles.css';
+import 'katex/dist/katex.min.css';
+import 'prismjs/themes/prism-tomorrow.css'
+import 'prismjs/components/prism-bash'
+import './App.css';
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+const ids = {
+  'git': '62f675e9dca84b8488f8db38344e175e',
+  'ml': 'a04548b962ae4d259c55e1d58dd59215',
+};
+
+class App extends React.Component<{}, { blockMap: any }> {
+
+  constructor(props: {}) {
+    super(props);
+    this.state = {
+      blockMap: undefined,
+    };
+  }
+
+  async componentDidMount() {
+    const response = await fetch(`https://notion-api.splitbee.io/v1/page/${ids['git']}`);
+    const blockMap = await response.json();
+    console.log(blockMap);
+    this.setState({ blockMap });
+  }
+
+  render() {
+    const { blockMap } = this.state;
+
+    return (
+      <main>
+        {blockMap ?
+          <NotionRenderer recordMap={{ block: blockMap } as any} components={{
+            Code,
+            Collection,
+            Equation,
+            Modal,
+            Pdf
+          }} fullPage={true} darkMode={true} /> :
+          <p>loading...</p>
+        }
+      </main>
+    )
+  }
 }
 
 export default App
