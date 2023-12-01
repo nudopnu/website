eb81301a1aed02ddf53308a075a0b541dad4ec6f
Peter Schramm <peter_schramm@live.de>
2023-12-01 20:27:06 +0100

    Further style ajustments

--- src/App.tsx
+++ src/App.tsx
@@ -10,14 +10,18 @@ import 'react-notion-x/src/styles.css';
 import 'katex/dist/katex.min.css';
 import 'prismjs/themes/prism-tomorrow.css'
 import 'prismjs/components/prism-bash'
+import 'prismjs/components/prism-yaml'
+import 'prismjs/components/prism-docker'
 import './App.css';
-import { Breadcrumb, ConfigProvider, theme } from "antd";
+import { ConfigProvider, theme } from "antd";
 import Header from "./components/Header";
 
 
 const ids = {
-  'git': '62f675e9dca84b8488f8db38344e175e',
-  'ml': 'a04548b962ae4d259c55e1d58dd59215',
+  git: '62f675e9dca84b8488f8db38344e175e',
+  ml: 'a04548b962ae4d259c55e1d58dd59215',
+  notes: '2b8b2bae1a7d4199b26e1479ef9cf351',
+  docker: '0e85c98660154326b950a51d2d148360',
 };
 
 class App extends React.Component<{}, { blockMap: any }> {
@@ -31,7 +35,7 @@ class App extends React.Component<{}, { blockMap: any }> {
 
   async componentDidMount() {
     if (this.state.blockMap !== undefined) return;
-    const response = await fetch(`https://notion-api.splitbee.io/v1/page/${ids['git']}`);
+    const response = await fetch(`https://notion-api.splitbee.io/v1/page/${ids.docker}`);
     const blockMap = await response.json();
     console.log(blockMap);
     this.setState({ blockMap });
@@ -55,7 +59,7 @@ class App extends React.Component<{}, { blockMap: any }> {
                 Equation,
                 Modal,
                 Pdf
-              }} fullPage={true} darkMode={true} header={<Header blockMap={blockMap}/>} disableHeader />
+              }} fullPage={true} darkMode={true} header={<Header blockMap={blockMap} />} disableHeader />
               :
               <p>loading...</p>
             }
