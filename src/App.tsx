4d61e6ddd8efffa5e4c8a406e9b7b4fa0eacee3e
Peter Schramm <peter_schramm@live.de>
2023-12-02 19:00:11 +0100

    Moved Notion to separate component

--- src/App.tsx
+++ src/App.tsx
@@ -1,72 +1,23 @@
 
 import React from "react";
-import { NotionRenderer } from 'react-notion-x';
-import { Code } from 'react-notion-x/build/third-party/code';
-import { Collection } from 'react-notion-x/build/third-party/collection';
-import { Equation } from 'react-notion-x/build/third-party/equation';
-import { Modal } from 'react-notion-x/build/third-party/modal';
-import { Pdf } from 'react-notion-x/build/third-party/pdf';
-import 'react-notion-x/src/styles.css';
-import 'katex/dist/katex.min.css';
-import 'prismjs/themes/prism-tomorrow.css'
-import 'prismjs/components/prism-bash'
-import 'prismjs/components/prism-yaml'
-import 'prismjs/components/prism-docker'
 import './App.css';
 import { ConfigProvider, theme } from "antd";
-import Header from "./components/Header";
+import Notion from "./components/Notion";
 
 
-const ids = {
-  git: '62f675e9dca84b8488f8db38344e175e',
-  ml: 'a04548b962ae4d259c55e1d58dd59215',
-  notes: '2b8b2bae1a7d4199b26e1479ef9cf351',
-  docker: '0e85c98660154326b950a51d2d148360',
-};
-
-class App extends React.Component<{}, { blockMap: any }> {
-
-  constructor(props: {}) {
-    super(props);
-    this.state = {
-      blockMap: undefined,
-    };
-  }
-
-  async componentDidMount() {
-    if (this.state.blockMap !== undefined) return;
-    const response = await fetch(`https://notion-api.splitbee.io/v1/page/${ids.git}`);
-    const blockMap = await response.json();
-    console.log(blockMap);
-    this.setState({ blockMap });
-  }
+class App extends React.Component {
 
   render() {
-    const { blockMap } = this.state;
+    const themeConfig = {
+      algorithm: theme.darkAlgorithm
+    };
 
     return (
-      <>
-        <ConfigProvider
-          theme={{
-            algorithm: theme.darkAlgorithm
-          }}
-        >
-          <main>
-            {blockMap ?
-              <NotionRenderer showTableOfContents={true} recordMap={{ block: blockMap } as any} components={{
-                Code,
-                Collection,
-                Equation,
-                Modal,
-                Pdf
-              }} fullPage={true} darkMode={true} header={<Header blockMap={blockMap} />} disableHeader />
-              :
-              <p>loading...</p>
-            }
-          </main>
-        </ConfigProvider>
-
-      </>
+      <ConfigProvider theme={themeConfig}>
+        <main>
+          <Notion pageId="ml" />
+        </main>
+      </ConfigProvider>
     )
   }
 }
---
+++ src/components/Notion.tsx
@@ -0,0 +1,61 @@
+
+import React from "react";
+import { NotionRenderer } from 'react-notion-x';
+import { Code } from 'react-notion-x/build/third-party/code';
+import { Collection } from 'react-notion-x/build/third-party/collection';
+import { Equation } from 'react-notion-x/build/third-party/equation';
+import { Modal } from 'react-notion-x/build/third-party/modal';
+import { Pdf } from 'react-notion-x/build/third-party/pdf';
+import 'react-notion-x/src/styles.css';
+import 'katex/dist/katex.min.css';
+import 'prismjs/themes/prism-tomorrow.css'
+import 'prismjs/components/prism-bash'
+import 'prismjs/components/prism-yaml'
+import 'prismjs/components/prism-docker'
+import { ConfigProvider, theme } from "antd";
+import Header from "./Header";
+import "./Notion.css";
+
+const ids = {
+    git: '62f675e9dca84b8488f8db38344e175e',
+    ml: 'a04548b962ae4d259c55e1d58dd59215',
+    notes: '2b8b2bae1a7d4199b26e1479ef9cf351',
+    docker: '0e85c98660154326b950a51d2d148360',
+};
+
+export default class Notion extends React.Component<{ pageId: keyof typeof ids }, { blockMap: any }> {
+
+    async componentDidMount() {
+        const response = await fetch(`https://notion-api.splitbee.io/v1/page/${ids[this.props.pageId]}`);
+        const blockMap = await response.json();
+        console.log(blockMap);
+        this.setState({ blockMap });
+    }
+
+    render() {
+        return (
+            <>
+                <ConfigProvider
+                    theme={{
+                        algorithm: theme.darkAlgorithm
+                    }}
+                >
+                    <main>
+                        {this.state ?
+                            <NotionRenderer showTableOfContents={true} recordMap={{ block: this.state.blockMap } as any} components={{
+                                Code,
+                                Collection,
+                                Equation,
+                                Modal,
+                                Pdf
+                            }} fullPage={true} darkMode={true} header={<Header blockMap={this.state.blockMap} />} disableHeader />
+                            :
+                            <p>loading...</p>
+                        }
+                    </main>
+                </ConfigProvider>
+
+            </>
+        )
+    }
+}
