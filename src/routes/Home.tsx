45785ed96d8fa717e48c1dc504a7845cbf9d9d79
Peter Schramm <peter_schramm@live.de>
2023-12-14 23:10:21 +0100

    Updated about page + favicon

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,23 +1,43 @@
-import { Avatar, Card, Divider } from "antd";
-import { ids } from "../contents/notion-pages";
+import { Card, Divider } from "antd";
 import Meta from "antd/es/card/Meta";
+import { Link as RouterLink } from "react-router-dom";
+import profile from '../assets/profile.jpg';
+import { ids } from "../contents/notion-pages";
 import './Home.css';
-import { Link } from "react-router-dom";
-import profile from '../assets/profile_small.jpg';
+import Link from "antd/es/typography/Link";
+import { MailOutlined } from '@ant-design/icons';
 
 export default function Home() {
     return (
         <>
-            <Avatar size={64} src={profile} />
             <main>
-                <h1>Peter Schramm</h1>
                 <section className="about">
+                    <div style={{ display: 'flex', alignItems: 'center' }}>
+                        <img src={profile} alt="profile picture" style={{ width: '130px', borderRadius: '10px', height: 'fit-content' }} />
+                    </div>
+                    <div>
+                        <h1>Peter Schramm</h1>
+                        <p>
+                            Software Developer from Germany
+                        </p>
+                        <p>
+                            Passionate about web-app development.
+                            Currently working as a scientific assistant
+                            at the University of Applied Sciences in Trier.
+                        </p>
+                        <p>
+                            <Link href="mailto:p.schramm@wir.hochschule-trier.de">
+                                <MailOutlined />
+                                <span> Contact me</span>
+                            </Link>
+                        </p>
+                    </div>
                 </section>
                 <Divider />
                 <h1>Notes</h1>
                 <section className="notes">
                     {Object.keys(ids).map((id) =>
-                        <Link to={`notes/${id}`} key={id}>
+                        <RouterLink to={`notes/${id}`} key={id}>
                             <Card
                                 hoverable
                                 style={{ width: 140, padding: 20, height: '180px' }}
@@ -27,7 +47,7 @@ export default function Home() {
                             >
                                 <Meta title={id} description={ids[id as keyof typeof ids].description} />
                             </Card>
-                        </Link>
+                        </RouterLink>
                     )}
                 </section>
             </main>
